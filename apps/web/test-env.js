45d4cb0d21cb0e82dbdbde05c3d547b74231679d
Dival Sehgal <sehgaldival@gmail.com>
2026-05-19 11:01:07 +0530

    chore: update test environment configuration and add @testing-library/dom dependency

--- apps/web/test-env.js
+++ apps/web/test-env.js
@@ -1,1 +1,3 @@
+/* eslint-disable no-console, @typescript-eslint/no-require-imports */
 console.log("CWD:", process.cwd(), "SPACE:", process.env.CONTENTFUL_SPACE_ID, "ENV_EXISTS:", require("fs").existsSync(".env"));
+
